4ba24a737cfcfb43a7c636f66d6dc40469a9e149
She <1443660195@qq.com>
2026-05-17 21:03:04 +0800

    创建选项界面完成，以及部分wxss完成

--- pages/make_choice/make_choice.js
+++ pages/make_choice/make_choice.js
@@ -4,8 +4,20 @@ Page({
     showDialogue: false,
     tempOption: "",
     showResult: false,
-    randomResult: null
+    randomResult: null,
+    optionIconsPath: [
+      {path: "../../img/options_icon/步数.png"},
+      {path: "../../img/options_icon/地址.png"},
+      {path: "../../img/options_icon/购物车.png"},
+      {path: "../../img/options_icon/浏览.png"},
+      {path: "../../img/options_icon/汽车.png"},
+      {path: "../../img/options_icon/文本.png"},
+      {path: "../../img/options_icon/饮食.png"},
+      {path: "../../img/options_icon/咨询.png"}
+    ],
+    tempIcon: ""
   },
+
 
 
   makeChoices() {
@@ -13,12 +25,13 @@ Page({
   },
 
 
+
   confirmChoice() {
     const userInput = this.data.tempOption
     if(userInput) {
       const newOption = {
         text: userInput,
-        icon: ""
+        icon: this.data.tempIcon
       }
       const newOptionList = this.data.optionList.concat([newOption])
       this.setData({
@@ -30,6 +43,7 @@ Page({
   },
 
 
+
   refuseChoice() {
     this.setData({
       showDialogue: false,
@@ -38,11 +52,13 @@ Page({
   },
 
 
+
   getChoice(choice) {
     this.setData({tempOption: choice.detail.value})
   },
 
   
+
   choose() {
     const list = this.data.optionList
     if(list.length != 0) {
@@ -55,7 +71,19 @@ Page({
   },
 
 
+
   goBack() {
     this.setData({showResult: false})
+  },
+
+
+
+  randomlyChooseIcon() {
+    const randomIconIndex = Math.floor(Math.random() * 8)
+    this.setData({tempIcon: this.data.optionIconsPath[randomIconIndex].path})
+  },
+  chooseIcon(e) {
+    const IconPath = e.currentTarget.dataset.image
+    this.setData({tempIcon: IconPath})
   }
 })
